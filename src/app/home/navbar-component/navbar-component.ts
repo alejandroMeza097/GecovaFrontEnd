72fe140a2a8b7615e25f6a432be593ac6a65abfb
IsaacGM00 <igmendez@minsait.com>
2025-07-21 20:09:37 -0600

    Se agrego boton de cerrar sesión y su respectiva funcionalidad en el navbar del home

--- src/app/home/navbar-component/navbar-component.ts
+++ src/app/home/navbar-component/navbar-component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-navbar-component',
   imports: [],
   templateUrl: './navbar-component.html',
-  styleUrl: './navbar-component.css'
+  styleUrl: './navbar-component.css',
 })
 export class NavbarComponent {
+  constructor(public auth: Auth) {}
 
+  logOut() {
+    this.auth.logout();
+  }
 }
